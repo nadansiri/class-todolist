955e9bd599e0717c4c1430cdf50c01dff7d81af1
Nissrine Hnainia <hnainianesrine@gmail.com>
2021-05-20 21:47:58 +0200

    using shortid

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ListGroup from "react-bootstrap/ListGroup";
-import Emoji from 'a11y-react-emoji'
+import Emoji from 'a11y-react-emoji';
+import shortid from "shortid";
 
 export default class TodoList extends Component {
     render() {
@@ -8,7 +9,7 @@ export default class TodoList extends Component {
             <div>
                 <ListGroup>
                     {this.props.tasks.map((el, key) =>
-                    <ListGroup.Item key={key}> 
+                    <ListGroup.Item key={shortid.generate()}> 
                         <Emoji symbol="✍"/>
                         {' '} 
                         {el.description}
